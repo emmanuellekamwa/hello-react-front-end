4ad16e0ae3c3924e13b621251ee3367880c22672
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2022-02-04 19:09:41 +0100

    Add stylelint file

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import getGreetingData from '../redux/api';
 import store from '../redux/configureStore';
+import './Greeting.css';
 
 store.dispatch(getGreetingData);
 
 const Greeting = () => {
   const randomGreeting = useSelector((state) => state.greetingData);
 
-  return <p>{randomGreeting}</p>;
+  return <p className="greeting">{randomGreeting}</p>;
 };
 
 export default Greeting;
